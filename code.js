fc036da33cd538b6a161b906ee7fb2a2ad4c7a77
Kim Michaux <kim.michaux@gmail.com>
2022-03-25 14:53:13 +0100

    playRound function added

--- code.js
+++ code.js
@@ -6,4 +6,32 @@ function computerPlay() {
     console.log(result);
     return result;
 }
-computerPlay();
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection) {
+        console.log("Draw");
+    } else if (playerSelection.toLowerCase() === "rock"){
+        if(computerSelection === "paper"){
+            console.log("You loose");
+        } else {
+            console.log("You win");
+        }
+    } else if(playerSelection.toLowerCase() === "paper"){
+        if (computerSelection === "rock"){
+            console.log("You loose");
+        } else{
+            console.log("You win");
+        }
+    } else if(playerSelection.toLowerCase() === "scissors"){
+        if (computerSelection === "rock"){
+            console.log("You loose");
+        }else{
+            console.log("You win");
+        }
+    }   
+}
+
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+playRound(playerSelection, computerSelection);
